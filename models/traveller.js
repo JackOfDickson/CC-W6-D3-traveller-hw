43b4a1c5b69a2c731ce86265fa679df0a66987dd
Jack <jackowendickson@gmail.com>
2022-03-30 18:49:31 +0100

    finished mvp

--- models/traveller.js
+++ models/traveller.js
@@ -11,23 +11,37 @@ Traveller.prototype.getJourneyStartLocations = function() {
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+  })
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport
+  })
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
-};
+  return this.journeys.filter((journey) => {
+    return journey.distance >= minDistance
+  })
+  };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce((total, journey) => {
+    return total + journey.distance;
+  },0)
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
-
+  let uniqueTransportArray = [];
+  this.journeys.forEach((journey) => {
+    if (!uniqueTransportArray.includes(journey.transport)) {
+      uniqueTransportArray.push(journey.transport)
+    }
+  })
+  return uniqueTransportArray
 };
 
 
